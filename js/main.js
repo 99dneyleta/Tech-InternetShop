39d21cc638708897b6e5672b902ddd554b062870
nastia_pavliuk <nas.pa1997@ukr.net>
2017-11-15 12:13:04 +0200

    start to do validation for register form

--- js/main.js
+++ js/main.js
@@ -22,3 +22,143 @@ $(function() {
     });
 
 });
+
+
+$(document).ready(function(){
+    $flag=1;
+    $("#username-reg").focusout(function(){
+        if($(this).val()===''){
+            $(this).css("border-color", "#FF0000");
+            $('#register-submit').attr('disabled',true);
+            $('#register-submit').css('color',"#565656");
+            $("#error_name").text("* You have to enter your name!");
+        }
+        else
+        {
+            $(this).css("border-color", "#2eb82e");
+            $('#register-submit').attr('disabled',false);
+            $('#register-submit').css('color',"white");
+            $("#error_name").text("");
+
+        }
+    });
+    $("#password-reg").focusout(function(){
+        $pass = $('#password-reg').val();
+        if($(this).val()===''){
+            $(this).css("border-color", "#FF0000");
+            $('#register-submit').attr('disabled',true);
+            $('#register-submit').css('color',"#565656");
+            $("#error_pass").text("* You have to enter your password!");
+        }
+        else
+        {
+            $(this).css("border-color", "#2eb82e");
+            $('#register-submit').attr('disabled',false);
+            $('#register-submit').css('color',"white");
+            $("#error_pass").text("");
+        }
+    });
+    $("#email").focusout(function(){
+        if($(this).val()===''){
+            $(this).css("border-color", "#FF0000");
+            $('#register-submit').attr('disabled',true);
+            $('#register-submit').css('color',"#565656");
+            $("#error_mail").text("* You have to enter your mail!");
+        }
+
+        else
+        {
+            $(this).css("border-color", "#2eb82e");
+            $('#register-submit').attr('disabled',false);
+            $('#register-submit').css('color',"white");
+            $("#error_mail").text("");
+        }
+    });
+    $("#confirm-password").focusout(function(){
+        if($(this).val()===''){
+            $(this).css("border-color", "#FF0000");
+            $('#register-submit').attr('disabled',true);
+            $('#register-submit').css('color',"#565656");
+            $("#error_confpass").text("* Confirm your password!");
+        }
+        else if ($(this).val()!==$pass)
+        {
+            $(this).css("border-color", "#FF0000");
+            $('#register-submit').attr('disabled',true);
+            $("#error_confpass").text("* Error");
+        }
+        else
+        {
+            $(this).css("border-color", "#2eb82e");
+            $('#register-submit').attr('disabled',false);
+            $('#register-submit').css('color',"white");
+            $("#error_confpass").text("");
+        }
+    });
+    $("#tel").focusout(function(){
+        $pho =$("#tel").val();
+        if($(this).val()===''){
+            $(this).css("border-color", "#FF0000");
+            $('#register-submit').attr('disabled',true);
+            $('#register-submit').css('color',"#565656");
+            $("#error_phone").text("* You have to enter your Phone Number!");
+        }
+        else if ($pho.length!==10)
+        {
+            $(this).css("border-color", "#FF0000");
+            $('#register-submit').attr('disabled',true);
+            $('#register-submit').css('color',"#565656");
+            $("#error_phone").text("* Lenght of Phone Number Should Be Ten");
+        }
+        else if(!$.isNumeric($pho))
+        {
+            $(this).css("border-color", "#FF0000");
+            $('#register-submit').attr('disabled',true);
+            $('#register-submit').css('color',"#565656");
+            $("#error_phone").text("* Phone Number Should Be Numeric");
+        }
+        else{
+            $(this).css({"border-color":"#2eb82e"});
+            $('#register-submit').attr('disabled',false);
+            $('#register-submit').css('color',"white");
+            $("#error_phone").text("");
+        }
+    });
+    $( "#submit" ).click(function() {
+        if($("#username-reg").val()==='')
+        {
+            $("#username-reg").css("border-color", "#FF0000");
+            $('#register-submit').css('color',"#565656");
+            $('#register-submit').attr('disabled',true);
+            $("#error_name").text("* You have to enter your first name!");
+        }
+        if($("#tel").val()==='')
+        {
+            $("#tel").css("border-color", "#FF0000");
+            $('#register-submit').attr('disabled',true);
+            $('#register-submit').css('color',"#565656");
+            $("#error_phone").text("* You have to enter your Phone Number!");
+        }
+        if($("#email").val()==='')
+        {
+            $("#email").css("border-color", "#FF0000");
+            $('#register-submit').attr('disabled',true);
+            $('#register-submit').css('color',"#565656");
+            $("#error_mail").text("* You have to enter your mail!");
+        }
+        if($("#password-reg").val()==='')
+        {
+            $("#password-reg").css("border-color", "#FF0000");
+            $('#register-submit').attr('disabled',true);
+            $('#register-submit').css('color',"#565656");
+            $("#error_pass").text("* You have to enter your Password!");
+        }
+        if($("#confirm-password").val()==='')
+        {
+            $("#confirm-password").css("border-color", "#FF0000");
+            $('#register-submit').attr('disabled',true);
+            $('#register-submit').css('color',"#565656");
+            $("#error_confpass").text("* Confirm your Password!");
+        }
+    });
+});
